b4e0ae0f7e235066de8e08e129d37db7d91831df
Jhon Brown <sefa.pehlivan@hepsiburada.com>
2025-07-19 17:32:08 +0300

    fix

--- sync_protos.sh
+++ sync_protos.sh
@@ -7,7 +7,7 @@ SRCS=(envoy contrib)
 IMPORT_BASE="github.com/CloudNativeWorks/versioned-go-control-plane"
 ENVOY_SRC_DIR="${ENVOY_SRC_DIR:-}"
 ENVOY_VERSION="${ENVOY_VERSION:-}"
-RATELIMIT_VERSION="${RATELIMIT_VERSION:-v0.1.0}"  # Default if not provided
+RATELIMIT_VERSION="${RATELIMIT_VERSION:-}"  # Will be set to match ENVOY_VERSION if not provided
 
 if [[ -z "$ENVOY_SRC_DIR" ]]; then
     echo "ENVOY_SRC_DIR not set, it should point to a cloned Envoy repo" >&2
@@ -46,6 +46,11 @@ sync_protos () {
         # Update import paths in all .go files
         find "${src}" -type f -name "*.go" -exec sed -i "s|github.com/envoyproxy/go-control-plane|${IMPORT_BASE}|g" {} \;
     done
+    
+    # Set ratelimit version to match envoy version if not provided
+    if [[ -z "$RATELIMIT_VERSION" ]]; then
+        RATELIMIT_VERSION="v${ENVOY_VERSION}"
+    fi
     
     # Create and update go.mod files
     printf "module github.com/CloudNativeWorks/versioned-go-control-plane\n\ngo 1.22\n\nreplace (\n  github.com/CloudNativeWorks/versioned-go-control-plane/envoy => ./envoy\n  github.com/CloudNativeWorks/versioned-go-control-plane/ratelimit => ./ratelimit\n)\n\nrequire (\n  github.com/CloudNativeWorks/versioned-go-control-plane/envoy v%s\n  github.com/CloudNativeWorks/versioned-go-control-plane/ratelimit %s\n  github.com/google/go-cmp v0.6.0\n  github.com/stretchr/testify v1.10.0\n  go.uber.org/goleak v1.3.0\n  google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53\n  google.golang.org/grpc v1.69.4\n  google.golang.org/protobuf v1.36.3\n)\n" "${ENVOY_VERSION}" "${RATELIMIT_VERSION}" > go.mod
